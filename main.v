9013bf738234ec0080ede90b9e3f7569834c7136
The127 <julian.baehr@googlemail.com>
2021-10-17 23:48:47 +0200

    create base for transpiler loop

--- main.v
+++ main.v
@@ -8,5 +8,20 @@ fn main() {
 
 	y_files := os.walk_ext(base_directory, '.y')
 
-	println(y_files)
+	generated_file_name := 'app.v'
+	// create empty file
+	if os.exists(generated_file_name) {
+		panic('generated file already exists')
+	}
+	mut f := os.create(generated_file_name) or { panic(err) }
+	f.close()
+
+	// iterate over all the y_files
+	for y_file in y_files {
+		transpile_y_file(y_file, generated_file_name)
+	}
+}
+
+fn transpile_y_file(y_file string, out_file string) {
+	println(y_file)
 }
